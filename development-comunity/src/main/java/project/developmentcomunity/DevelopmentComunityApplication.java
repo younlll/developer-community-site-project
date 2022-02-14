2db233661473ad866894a1e742a154175f3ed1f6
younlll <youn.lee96@gmail.com>
2022-02-15 00:25:37 +0900

    feat: create project

---
+++ development-comunity/src/main/java/project/developmentcomunity/DevelopmentComunityApplication.java
@@ -0,0 +1,13 @@
+package project.developmentcomunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentComunityApplication.class, args);
+	}
+
+}

e7e1b9392606e03466ef77ebd88d381849312379
younlll <youn.lee96@gmail.com>
2022-02-19 22:19:02 +0900

    test: Test code modification-DB interworking

--- development-comunity/src/test/java/project/developmentcomunity/service/UserServiceTest.java
+++ development-comunity/src/test/java/project/developmentcomunity/service/UserServiceTest.java
@@ -1,15 +1,19 @@
 package project.developmentcomunity.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import project.developmentcomunity.domain.User;
 import project.developmentcomunity.repository.UserRepository;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+@SpringBootTest
+@Transactional
 class UserServiceTest {
 
     @Autowired UserService userService;
@@ -17,16 +21,14 @@ class UserServiceTest {
 
     @Test
     void 회원번호_채번() {
-        int userId = userService.inqMaxUserId().get();
-        List<User> findUserId = userService.inqUserId(userId).get();
-
-        assertThat(findUserId.size()).isEqualTo(0);
+        long userId = userService.numberingUserId();
+        assertThat(userService.inqUserId(userId).isPresent()).isEqualTo(false);
     }
 
     @Test
     void 회원가입() {
         User user = new User();
-        int userId = userService.inqMaxUserId().get();
+        long userId = userService.numberingUserId();
         user.setIdUser(userId);
         user.setName("이유나");
         user.setPassword("password");
@@ -35,14 +37,14 @@ class UserServiceTest {
 
         userService.joinUser(user);
 
-        List<User> findUserId = userService.inqUserId(userId).get();
-        assertThat(findUserId.get(0).getEmail()).isEqualTo(user.getEmail());
+        User findUser = userService.inqUserEmail(user.getEmail()).get();
+        assertThat(user.getEmail()).isEqualTo(findUser.getEmail());
     }
 
     @Test
     void 중복회원_예외() {
         User user1 = new User();
-        int userId1 = userService.inqMaxUserId().get();
+        long userId1 = userService.numberingUserId();
         user1.setIdUser(userId1);
         user1.setName("이유나");
         user1.setPassword("password");
@@ -50,7 +52,7 @@ class UserServiceTest {
         user1.setEmail("youn.lee96@gmail.com");
 
         User user2 = new User();
-        int userId2 = userService.inqMaxUserId().get();
+        long userId2 = userService.numberingUserId();
         user2.setIdUser(userId2);
         user2.setName("이유나");
         user2.setPassword("password");

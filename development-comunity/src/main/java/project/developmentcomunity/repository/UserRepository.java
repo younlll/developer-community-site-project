70a872515d55d912fc150cac81a30d367c7e3024
younlll <youn.lee96@gmail.com>
2022-03-31 23:57:40 +0900

    refactor: userrepository 수정

--- development-comunity/src/main/java/project/developmentcomunity/repository/UserRepository.java
+++ development-comunity/src/main/java/project/developmentcomunity/repository/UserRepository.java
@@ -2,22 +2,21 @@ package project.developmentcomunity.repository;
 
 import project.developmentcomunity.domain.User;
 
-import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 public interface UserRepository {
 
     // 회원가입
-    void joinUser(User user);
+    void save(User user);
 
     // 회원ID 채번
     Long inqMaxUserId();
 
     // 회원Email 조회
-    Optional<User> inqUserEmail(String userEmail);
+    boolean findByEmail(String email);
 
     // 회원ID 조회
-    Optional<User> inqUserId(Long userId);
+    Optional<User> findById(Long userId);
 
     // 회원 password 조회
     String inqUserPassword(String userEmail);
